9756f4e9e7f7d2a2644412b0f8b9403f2baead81
B1N4R1 <gorkfern07@gmail.com>
2020-11-24 23:38:02 +0100

    draw objects & start with some animations

--- js/script.js
+++ js/script.js
@@ -1,20 +1,59 @@
 
-window.requestAnimFrame = (function (){
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+let score = 0;
+
+const brickRows = 7;
+const brickCols = 4;
+
+var paddle = {
+    x: (canvas.width - 65)/2,
+    y: canvas.height - 30,
+    width: 65,
+    height: 10,
+    color: "#3498db"
+}
+
+var circle = {
+    x: 200,
+    y: 200,
+    size: 10,
+    dx: 5,
+    dy: 4,
+    color: "#3498db"
+}
+
+var brickInfo = {
+    width: 70,
+    height: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 50,
+    visible: true
+}
+
+const bricks = [];
+for (let i = 0; i < brickRows; i++) {
+    
+    bricks[i] = [];
+    for (let j = 0; j < brickCols; j++) {
         
-        return window.requestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.oRequestAnimationFrame || 
-                window.msRequestAnimationFrame ||
-               function (callback){
-                    window.setTimeout(callback, 1000 / 60);
-                };
+        const x = i * (brickInfo.width + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.height + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = {x, y, ...brickInfo}
+        
+    }
+    
+}
 
-})();
+var animProp = {
 
-function drawCircle(circle, canvas) {
+    status: false
 
-    var context = canvas.getContext("2d");
+};
+
+function drawCircle() {
 
     context.beginPath();
     context.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
@@ -23,28 +62,67 @@ function drawCircle(circle, canvas) {
   
 }
 
-function drawSquare(){
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.moveTo(5, 5);
-    ctx.lineTo(635, 395);
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
+function drawPaddle(){
+
+    context.beginPath();
+    context.fillStyle = paddle.color;
+    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    context.closePath();
 }
 
-function animate(circle, animProp, canvas){
+function drawBricks(){
+    bricks.forEach(col => {
+        col.forEach(brick => {
+            context.beginPath();
+            context.rect(brick.x, brick.y, brick.width, brick.height);
+            context.fillStyle = brick.visible ? '#3498db' : 'transparent';
+            context.fill();
+            context.closePath();
+        })
+    })
+}
+
+function drawScore(){
+    
+    context.font = "20px Arial";
+    context.fillText(`Score: ${score}`, canvas.width - 100, 30);
+
+}
+
+function draw(){
+
+    drawCircle();
+    drawPaddle();
+    drawBricks();
+    drawScore();
+
+}
+
+function animate(){
 
     if (animProp.status) {
-
-        var context = canvas.getContext("2d");
 
         // clear  
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         // draw  
-        drawCircle(circle, canvas);
+        draw(paddle, circle, canvas);
 
-        // movement
+        //paddle movement
+        canvas.addEventListener('mousedown', e => {
+            paddle.x = e.offsetX;
+        });
+
+        canvas.addEventListener('mousemove', e => {
+            paddle.x = e.offsetX;
+        });
+
+        canvas.addEventListener('mouseup', e => {
+            paddle.x = e.offsetX;
+        });
+        
+
+        // circle movement
         circle.x += circle.dx;
         circle.y += circle.dy;
     
@@ -66,71 +144,58 @@ function animate(circle, animProp, canvas){
 
         }
 
-        // request new frame  
-        requestAnimFrame(function (){
 
-            animate(circle, animProp, canvas);
+        // Detect paddle
+        
+        if (circle.x - circle.size > paddle.x - paddle.width && 
+            circle.x + circle.size < paddle.x + paddle.width && 
+            circle.y + circle.size > paddle.y
+        ) {
+            
+           circle.dy *= -1;
 
-        });
+        }
+
+        requestAnimationFrame(animate);
 
     }  
 }
 
-drawSquare();
+function randColor(){
+ 
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
 
-// function randColor(){
-    
-//     return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
 
-// }
+window.onload = function (){
 
-// window.onload = function (){
+    // add click listener to canvas  
+    document.getElementById("button").addEventListener("click", function (){
 
-//     var canvas = document.getElementById("myCanvas");
+        var btn = this.getAttribute("data-btn");
 
-//     var circle = {
-//         x: 200,
-//         y: 200,
-//         size: 30,
-//         dx: 5,
-//         dy: 4,
-//         color: "#3498db"
-//     }
+        if (animProp.status){
 
-//     var animProp = {
+            animProp.status = false;
 
-//         status: false
+            document.getElementById("button").firstChild.data = btn.toUpperCase();
 
-//     };
+            document.getElementById("button").setAttribute("data-btn", "stop");
 
+        }  
+        else{
 
-//     // add click listener to canvas  
-//     document.getElementById("button").addEventListener("click", function (){
+            document.getElementById("button").firstChild.data = btn.toUpperCase();
 
-//         var btn = this.getAttribute("data-btn");
+            document.getElementById("button").setAttribute("data-btn", "start");
 
-//         if (animProp.status){
+            animProp.status = true;   
+            animate();
 
-//             animProp.status = false;
+        }
 
-//             document.getElementById("button").firstChild.data = btn.toUpperCase();
+    });
 
-//             document.getElementById("button").setAttribute("data-btn", "stop");
+    draw();
 
-//         }  
-//         else{
-
-//             document.getElementById("button").firstChild.data = btn.toUpperCase();
-
-//             document.getElementById("button").setAttribute("data-btn", "start");
-
-//             animProp.status = true;   
-//             animate(circle, animProp, canvas);
-
-//         }
-
-//     });
-
-//     drawCircle(circle, canvas);
-
-// };  
+};  
